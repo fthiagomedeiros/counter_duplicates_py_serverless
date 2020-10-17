bc98dc0281aee73f38a72cf38fa13dc0716f7889
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-10-16 22:12:32 -0300

    some unit tests

--- tests/business_logic_test/test_counter_duplicates.py
+++ tests/business_logic_test/test_counter_duplicates.py
@@ -1,3 +1,13 @@
-# from counter_duplicates import counter_duplicates
-#
-# assert counter_duplicates('ccbb') == {'c': 2, 'b': 2}
+import pytest
+
+from src.business_logic.counter_duplicates import counter_duplicates
+
+
+@pytest.mark.parametrize("name,expected", [("teste", {"t": 2, "e": 2}),
+                                           ("wwook", {"w": 2, "o": 2}),
+                                           ("         ", {}),
+                                           ("", {}),
+                                           ])
+def test_find_duplicates(name, expected):
+    duplicates_output = counter_duplicates(name)
+    assert duplicates_output == expected
