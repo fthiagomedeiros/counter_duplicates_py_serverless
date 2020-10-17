a5b75d739234f89e30e2da0952877157c7fe18e3
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-10-16 22:18:05 -0300

    some unit tests parametrized

--- tests/business_logic_test/test_counter_duplicates.py
+++ tests/business_logic_test/test_counter_duplicates.py
@@ -5,10 +5,10 @@ from src.business_logic.counter_duplicates import counter_duplicates
 
 @pytest.mark.parametrize("name,expected", [("teste", {"t": 2, "e": 2}),
                                            ("wwook", {"w": 2, "o": 2}),
-                                           ("", {}),
                                            ("abcd", {}),
                                            ("         ", {}),
                                            ("Brazil", {}),
+                                           ("", {})
                                            ])
 def test_find_duplicates(name, expected):
     duplicates_output = counter_duplicates(name)
